50532942615c35a0f1c4ffac07679b5e3051d054
Frank Pope <fpope.sodexo@gmail.com>
2025-02-26 10:13:56 -0700

    Added a basic static main to test pushes to main

---
+++ src/propertynmoney/GitPushTester.java
@@ -0,0 +1,11 @@
+package propertynmoney;
+
+import java.util.Random;
+
+public class GitPushTester {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt(100);
+        System.out.println("Random number: " + randomNum);
+    }
+}
