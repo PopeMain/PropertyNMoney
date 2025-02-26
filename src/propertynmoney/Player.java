0577dbdd75b2afa2360d269506b60a364bed3586
Frank Pope <fpope.sodexo@gmail.com>
2025-02-26 10:17:52 -0700

    Save the build for the PopeTests

--- src/propertynmoney/Player.java
+++ src/propertynmoney/Player.java
@@ -2,15 +2,20 @@ package propertynmoney;
 
 public abstract class Player {
     int money;
+    int position;
     String name;
 
     public Player(int money, String name) {
         this.money = money;
         this.name = name;
+        this.position = 0;
     }
 
     public int getMoney() {return money;}
     public String getName() {return name;}
 
     public void addMoney(int money) {this.money += money;}
+    public void subMoney(int money) {this.money -= money;}
+
+
 }
