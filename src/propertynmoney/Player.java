9a32e0bae8f0b16c8513d91e54f0466c2cae0cff
Frank Pope <fpope.sodexo@gmail.com>
2025-02-27 18:50:52 -0700

    Added a test for the player Class.

--- src/propertynmoney/Player.java
+++ src/propertynmoney/Player.java
@@ -1,14 +1,16 @@
 package propertynmoney;
 
-public abstract class Player {
+public class Player {
     int money;
     int position;
     String name;
+    boolean inJail;
 
     public Player(int money, String name) {
         this.money = money;
         this.name = name;
         this.position = 0;
+        this.inJail = false;
     }
 
     public int getMoney() {return money;}
@@ -16,6 +18,11 @@ public abstract class Player {
 
     public void addMoney(int money) {this.money += money;}
     public void subMoney(int money) {this.money -= money;}
+
+    public int getPosition() {return position;}
+    public void movePosition(int rollValue){
+        position = (position + rollValue) % 40;
+    }
 
 
 }
--- src/propertynmoney/Tile.java
+++ src/propertynmoney/Tile.java
@@ -4,20 +4,6 @@ import propertynmoney.Property.*;
 
 
 public abstract class Tile {
-    Node head;
-
-    private static class Node {
-        Property data;
-        Node next;
-
-        Node(Property data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-//
-//    public Tile() {}
-//
-//    public nodeGetData(){}
-//
+    Player[] players;
+    enum tileType {PROPERTY, CHANCE, COMCHEST, JAIL, GO}
 }
