d6a8fe916c113c3cddd67cd9d841a6f220752d4f
Frank Pope <fpope.sodexo@gmail.com>
2025-03-19 10:13:05 -0600

    Adding a Bank Class, This will be the cardinal controller for economics of the board game

---
+++ src/propertynmoney/Bank.java
@@ -0,0 +1,14 @@
+package propertynmoney;
+
+import propertynmoney.Property;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+    private List<Property> properties;
+
+    public Bank(List<Property> properties) {
+        this.properties = new ArrayList<Property>();
+    }
+}
